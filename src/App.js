ae52e5b804b3f87fecc9d38a82954e94e8362d47
RAJ <rajumacha@yahoo.com>
2020-08-10 16:51:33 +0530

    removed unused uuid import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ToDoInput from './ToDoList/components/ToDoInput';
 import ToDoList from './ToDoList/components/ToDoList';
-import uuid from 'uuid';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
